15fd18d890142fa26968157cd9a7759e9bd1bbee
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-02-14 21:22:41 +0530

    added a comment

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
+//creating a server
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
