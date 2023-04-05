3820351ae8a963b45a9febe10e0b1ef71aa3f2be
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-04-06 01:36:18 +0530

    added controller for update and delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,6 +13,50 @@ exports.createProduct = async (req, res, next) => {
 //GET ALL Product
 
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+  const products = await Product.find(); //gets all products
   res.status(200).json({ success: true, products });
+};
+
+//Update Product
+exports.updateProduct = async (req, res) => {
+  const id = req.params.id;
+  const update = req.body;
+  const options = { new: true, runValidators: true, useFindAndModify: false };
+
+  let product = await Product.findById(id);
+
+  //check if product already exists or not
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(id, update, options); //options returns the update product
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//DELETE PRODUCT
+exports.deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product removed successfully",
+  });
 };
