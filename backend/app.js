9ad480bed8aa63746b35b306cebe11a85f409c30
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-04-08 01:31:59 +0530

    added error middleware to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 
@@ -11,5 +12,8 @@ app.use("/api/v1", product);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//Middleware for Errors
+app.use(errorMiddleware);
 
 module.exports = app;
