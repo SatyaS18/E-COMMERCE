2a73b89bfe1ef2aea2fb25d841a931afc9889581
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-04-08 03:36:43 +0530

    made some changes

--- backend/middleware/catchAsyncErrors.js
+++ backend/middleware/catchAsyncErrors.js
@@ -3,3 +3,4 @@ module.exports = (theFunc) => (req, res, next) => {
 };
 
 //this middleware is used in case there are missing inputs for the async function of the controllers
+// if an error occurs during an asynchronous operation, the error will be thrown outside the normal flow of execution, and the application may crash or behave unexpectedly.
